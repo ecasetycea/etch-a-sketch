e89b4a16eea39694dceacbcbbe00e1dd654f8f49
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-01 09:31:46 +0300

    Make grid square to fixed size

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const GRID_HEIGHT = 16;
-const GRID_WIDTH = 16;
+const GRID_RESOLUTION = [16, 16]; //[width, height]
+const GRID_WIDTH = 960; //px
 
 
 createGrid();
@@ -7,18 +7,32 @@ createGrid();
 
 function createGrid() {
     const gridContainer = document.querySelector(".gridContainer");
-    
-    for(let i=0; i<GRID_WIDTH; i++) {
+    const boxSize = getBoxSize() + "px";
+
+    for(let i=0; i<GRID_RESOLUTION[0]; i++) {
         let gridColumn = document.createElement("div");
         gridColumn.classList.add("gridColumn");
         
-        for(let j=0; j<GRID_HEIGHT; j++) {
+        for(let j=0; j<GRID_RESOLUTION[1]; j++) {
             let gridBox = document.createElement("div");
             gridBox.classList.add("gridBox");
             gridBox.textContent = String(j) + ',' + String(i);
-            
+            gridBox.style.width = boxSize;
+            gridBox.style.height = boxSize;
+
             gridColumn.appendChild(gridBox);
         }
         gridContainer.appendChild(gridColumn);
     }
+}
+
+function getBoxSize() {
+    let out = Math.max(...GRID_RESOLUTION);
+    out = GRID_WIDTH / out;
+    out = Math.floor(out);
+    return out; 
+}
+
+function resizeBoxes() {
+    //dynamically scale container with viewport
 }
