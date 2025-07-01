551d13fe9ec905218af22877882c08baca9b6967
ecasetycea <15278813+ecasetycea@users.noreply.github.com>
2025-07-01 10:17:54 +0300

    Implement mouse trace

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
-const GRID_RESOLUTION = [16, 16]; //[width, height]
-const GRID_WIDTH = 960; //px
+const GRID_RESOLUTION = [20, 16]; //[width, height]
+const GRID_SIZE = [960, 960]; //[width, height] (px)
 
+const gridContainer = document.querySelector(".gridContainer");
 
 createGrid();
 
 
 function createGrid() {
-    const gridContainer = document.querySelector(".gridContainer");
+    setGridSize(...GRID_SIZE);
     const boxSize = getBoxSize() + "px";
 
+    //loop columns
     for(let i=0; i<GRID_RESOLUTION[0]; i++) {
         let gridColumn = document.createElement("div");
         gridColumn.classList.add("gridColumn");
         
+        //loop rows
         for(let j=0; j<GRID_RESOLUTION[1]; j++) {
             let gridBox = document.createElement("div");
             gridBox.classList.add("gridBox");
-            gridBox.textContent = String(j) + ',' + String(i);
             gridBox.style.width = boxSize;
             gridBox.style.height = boxSize;
+            
+            //for debug
+            gridBox.textContent = String(j) + ',' + String(i);
+            
+            //add border missing from some boxes
+            if(j === 0) gridBox.classList.add("isOnTopRow");
+            if(i === 0) gridBox.classList.add("isOnLeftColumn");
 
             gridColumn.appendChild(gridBox);
         }
         gridContainer.appendChild(gridColumn);
     }
+
+    addEL();
+}
+
+function addEL() {
+    gridContainer.addEventListener( "mouseover", (e) => {
+        e.target.classList.add("isColored");
+    });
+}
+
+function setGridSize(width, height) {
+    gridContainer.style.width = width + "px";
+    gridContainer.style.height = height + "px";
 }
 
 function getBoxSize() {
-    let out = Math.max(...GRID_RESOLUTION);
-    out = GRID_WIDTH / out;
-    out = Math.floor(out);
-    return out; 
+    let maxRes = Math.max(...GRID_RESOLUTION);
+    let minSize = Math.min(...GRID_SIZE);
+    let boxSide = minSize / maxRes;
+    return Math.floor(boxSide);
 }
 
 function resizeBoxes() {
